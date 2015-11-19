c4b1b5a6b7c67d68af84ad781696fb40c5f68231
rezashnia1234 <reza_shnia@spam4.me>
2015-11-19 18:27:08 +0330

    0000

---
+++ src/android/Share.java
@@ -0,0 +1,28 @@
+package nl.madebymark.share;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.widget.Toast;
+
+import android.content.Intent;
+
+    /**
+     * This class echoes a string called from JavaScript.
+     */
+    public class Share extends CordovaPlugin {
+
+        @Override
+        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+            if (action.equals("share")) {
+                Toast.makeText(this.cordova.getActivity().getApplicationContext(), "test taost from me", Toast.LENGTH_LONG).show();
+                return true;
+            }
+            return false;
+        }
+
+        
+    }
